ac32ec2d81bf90c2dc6bb95408aec7be864f04e2
Lillian Luzinsky <eyeris@tpg.com.au>
2019-12-18 16:11:34 +1100

    Fixed Modal

--- src/components/Cart/PurchasePage.js
+++ src/components/Cart/PurchasePage.js
@@ -3,57 +3,45 @@ import React, { Component } from 'react';
 import { FoodTextLayout, FoodImage, FoodName, FoodCost } from '../Food/FoodStyled';
 import { PageSize, AquaBanner, PinkContainer, YellowBanner, PurchaseCard, AddSubtractBlock } from './PurchasePageStyled';
 import { Add, Subtract, Button } from '../Button';
+import {BackDrop} from '../Modal/ModalStyled';
+
+import { Modal } from '../Modal/Modal';
 
 export default class PurchasePage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isShowing: false
+    }
+  }
+
+  openModalHandler = () => {
+    this.setState({
+      isShowing: true
+    });
+  }
+
+  closeModalHandler = () => {
+    this.setState({
+      isShowing: false
+    });
+  }
+
   render() {
     return (
       <PageSize>
+        
+
         <PinkContainer>
-          <PurchaseCard>
+          <Modal
+            className="modal"
+            show={this.state.isShowing}
+            close={this.closeModalHandler}>
+            <p>and thanks for shopping with us</p>
 
-            <FoodImage/>
+          </Modal>
 
-            <FoodTextLayout>
-              <FoodName>Iced Coffee</FoodName>
-              <FoodCost>... $5.00</FoodCost>
-            </FoodTextLayout>
-
-            <AddSubtractBlock>
-              <Add>+</Add>
-              <Subtract>-</Subtract>
-            </AddSubtractBlock>
-            
-          </PurchaseCard>
-          <PurchaseCard>
-
-            <FoodImage />
-
-            <FoodTextLayout>
-              <FoodName>Iced Coffee</FoodName>
-              <FoodCost>... $5.00</FoodCost>
-            </FoodTextLayout>
-
-            <AddSubtractBlock>
-              <Add>+</Add>
-              <Subtract>-</Subtract>
-            </AddSubtractBlock>
-
-          </PurchaseCard>
-          <PurchaseCard>
-
-            <FoodImage />
-
-            <FoodTextLayout>
-              <FoodName>Iced Coffee</FoodName>
-              <FoodCost>... $5.00</FoodCost>
-            </FoodTextLayout>
-
-            <AddSubtractBlock>
-              <Add>+</Add>
-              <Subtract>-</Subtract>
-            </AddSubtractBlock>
-
-          </PurchaseCard>
           <PurchaseCard>
 
             <FoodImage />
@@ -74,8 +62,12 @@ export default class PurchasePage extends Component {
         </PinkContainer>
         <AquaBanner>
           <h1>Total Amount: $000</h1>
-          <Button>Confirm Purchase</Button>
+
+          {this.state.isShowing ? <BackDrop onClick={this.closeModalHandler}></BackDrop> : null}
+
+          <Button onClick={this.openModalHandler}>Confirm Purchase</Button>
         </AquaBanner>
+        
       </PageSize>
     );
   }
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '../Button';
+import {ModalWrapper} from './ModalStyled';
+
+export const Modal = (props) => {
+  return (
+      <ModalWrapper
+        style={{
+          transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}>
+
+          <h1>Enjoy Your Sweets</h1>
+          <p>Thanks for shopping with us</p>
+
+          <Button onClick={props.close}>Close</Button>
+
+    </ModalWrapper>
+  )
+};
