656c9c9430c6a09685f3b8ff3ecd87166206c0c5
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-14 10:02:46 +1100

    deleted blue outline when pressing a button and added an emoji

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,19 +4,18 @@ import {ModalWrapper, ModalText} from './ModalStyled';
 
 export const Modal = (props) => {
   return (
-      <ModalWrapper
-        style={{
-          transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-
-          <ModalText>
-            <h1 id="modalTitle">Enjoy Your Sweets</h1>
-            <p id="modalParr">Thanks for shopping with us</p>
-          </ModalText>
-          
+    <ModalWrapper
+      style={{
+        transform: props.show ? "translateY(0vh)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0"
+      }}
+    >
+      <ModalText>
+        <h1 id="modalTitle">Enjoy Your Sweets</h1>
+        <p id="modalParr">Thanks for shopping with us &#x1F600;</p>
+      </ModalText>
 
       <ModalButton onClick={props.close}>Close</ModalButton>
     </ModalWrapper>
-  )
+  );
 };
