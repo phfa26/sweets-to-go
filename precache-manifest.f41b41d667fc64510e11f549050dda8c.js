aafd319ccf7b20bcbc228a3da6756061764cbe8e
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-05 14:52:50 +1100

    Updates

---
+++ precache-manifest.f41b41d667fc64510e11f549050dda8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ac37d0d340b2753f1fa49373c3ba9b",
+    "url": "/sweets-to-go/index.html"
+  },
+  {
+    "revision": "f473d0a7e32900ddaffa",
+    "url": "/sweets-to-go/static/css/main.425fa203.chunk.css"
+  },
+  {
+    "revision": "cbcb245d2d72962a4ee6",
+    "url": "/sweets-to-go/static/js/2.beeb7c1d.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/sweets-to-go/static/js/2.beeb7c1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f473d0a7e32900ddaffa",
+    "url": "/sweets-to-go/static/js/main.643c6562.chunk.js"
+  },
+  {
+    "revision": "a07e2ed462be5d34225a",
+    "url": "/sweets-to-go/static/js/runtime-main.1dd143aa.js"
+  }
+]);
